4e517e1ec9cc531deebfe1fa724560184a9e37f9
Ventura <ventura.py3@gmail.com>
2024-07-16 22:56:21 -0300

    First board classes implemented

---
+++ chess-console/board/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace board
+{
+    internal class Board
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        private Piece[,] _pieces;
+
+        public Board(int row, int column)
+        {
+            Row = row;
+            Column = column;
+            _pieces = new Piece[row, column];
+        }
+    }
+}
